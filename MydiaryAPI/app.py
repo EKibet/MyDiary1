995a0a6986a08035139d68e411d67b7d929cc6a8
Edgar Kibet <kibetedgar@gmail.com>
2018-07-18 14:09:50 +0300

    Defines two URLs /entries for the list of entries and /entries/<int:id> for individual entry

--- MydiaryAPI/app.py
+++ MydiaryAPI/app.py
@@ -4,13 +4,19 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class DiaryAPI(Resource):
+class EntryListAPI(Resource):
     def get(self, id):
         pass
     def getOne(self, id):
+        pass
+
+
+class EntryAPI(Resource):
+    def get(self, id):
         pass
     def put(self, id):
         pass
     def post(self, id):
         pass
-api.add_resource(DiaryAPI, '/entries', endpoint = 'entry')
+api.add_resource(EntryListAPI, '/entries', endpoint = 'entries')
+api.add_resource(EntryAPI, '/entries/<int:id>', endpoint = 'entry')
