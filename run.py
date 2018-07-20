1bb744867c16bc1590c2c5708e02741571f28d2d
Edgar Kibet <kibetedgar@gmail.com>
2018-07-20 13:08:55 +0300

    [Feature #159133731] sets up the app

--- run.py
+++ run.py
@@ -3,14 +3,9 @@
 """RESTful server implemented using the
 Flask-RESTful extension."""
 
-from flask import Flask, jsonify, abort, make_response
-from flask_restful import Api, Resource, reqparse, fields, marshal
-from flask_httpauth import HTTPBasicAuth
-from app.app import *
+import os
+from app.app import create_app
 
-api.add_resource(Entries_API, '/diary/api/v1.0/entries', endpoint='entries')
-api.add_resource(EntryAPI, '/diary/api/v1.0/entries/<int:id>', endpoint='entry')
-
-
-if __name__ == '__main__':
+app = create_app('development')
+if __name__ =='__main__':
     app.run(debug=True)
